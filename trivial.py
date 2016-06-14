408858fd17e91885f4c3b0edd67fe9bb7b45ebd6
manuelalcocer <m.alcocer1978@gmail.com>
2016-06-14 09:16:46 +0200

    debug mode

--- trivial.py
+++ trivial.py
@@ -322,8 +322,8 @@ def Fetch_Question():
 
 def Load_Game():
     global trivial,OPTS
-    trivial['buffer_ptr'] = weechat.buffer_search('irc','%s.%s' %(OPTS['plugin_options']['server']), OPTS['plugin_options']['room']))
-    weechat.prnt('', 'hit: %s %s %s' %(str(trivial['buffer_ptr'], OPTS['plugin_options']['server']), OPTS['plugin_options']['room'])))
+    trivial['buffer_ptr'] = weechat.buffer_search('irc','%s.%s' %(OPTS['plugin_options']['server'], OPTS['plugin_options']['room']))
+    weechat.prnt('', 'hit: %s %s %s' %(str(trivial['buffer_ptr'], OPTS['plugin_options']['server'], OPTS['plugin_options']['room'])))
     if not trivial.has_key('running'):
         trivial['running'] = False
 
