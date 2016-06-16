9848d91b3a01978d2bc80a97b464e02a151e1afe
manuelalcocer <m.alcocer1978@gmail.com>
2016-06-16 09:32:02 +0200

    beginning of msg stop bug

--- trivial.py
+++ trivial.py
@@ -556,12 +556,12 @@ def Check_message_cb(data, buffer, date, tags, displayed, highlight, prefix, mes
     if TRIV['instances']['launched'][data].running == True and TRIV['instances']['launched'][data].trivial['state'] != 0:
         if message.lower() == TRIV['instances']['launched'][data].answer.lower():
             TRIV['instances']['launched'][data].Winner(nick)
-    if message.lower() == cmd_prefix + 'trivial stop' and TRIV['instances']['launched'][data].Is_Admin(nick):
+    if message.lower() == cmd_prefix + 'trivial stop' and TRIV['instances']['launched'][data].Is_Admin(nick) and buffer == TRIV['instances']['launched'][data].buffer_ptr:
         #TRIV['instances']['launched'][data].Stop_Game()
         weechat.prnt('', '/trivial stop %s' %data)
         weechat.prnt('', 'opp: %s' %str(TRIV['instances']['launched']))
         weechat.command(buffer, '/trivial stop %s' %data)
-    elif message.lower() == cmd_prefix + 'trivial start' and TRIV['instances']['launched'][data].Is_Admin(nick):
+    elif message.lower() == cmd_prefix + 'trivial start' and TRIV['instances']['launched'][data].Is_Admin(nick) and buffer == TRIV['instances']['launched'][data].buffer_ptr:
         #TRIV['instances']['launched'][data].Start_Game()
         weechat.prnt('', '/trivial start %s' %data)
         weechat.prnt('', 'opp: %s' %str(TRIV['instances']['launched']))
